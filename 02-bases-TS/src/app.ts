feeb3cc75026cc5d63e3b352c0db4173c8608ebc
Ulises Desentis <ulisesdesentis@MacBook-Air-de-Ulises.local>
2025-02-26 17:51:26 -0600

    init testing

--- 02-bases-TS/src/app.ts
+++ 02-bases-TS/src/app.ts
@@ -1,15 +1,15 @@
-import { getPokemon } from './js-foundation/06-promises';
+// import { getPokemon } from './js-foundation/06-promises';
 
-import { buildLogger } from "./plugins/logger.plugin";
+// import { buildLogger } from "./plugins/logger.plugin";
 
-const logger = buildLogger('app.js');
+// const logger = buildLogger('app.js');
 
-logger.log('Starting app.js');
+// logger.log('Starting app.js');
 
-getPokemon(12)
-  .then((pokemon) => console.log(pokemon.name))
-  //.catch((error) => console.error('Pokemon no encontrado', error))
-  .catch((error) => logger.error('Pokemon no encontrado'))
-  .finally(() => console.log('Pokemon fetched'));
+// getPokemon(12)
+//   .then((pokemon) => console.log(pokemon.name))
+//   //.catch((error) => console.error('Pokemon no encontrado', error))
+//   .catch((error) => logger.error('Pokemon no encontrado'))
+//   .finally(() => console.log('Pokemon fetched'));
 
 
